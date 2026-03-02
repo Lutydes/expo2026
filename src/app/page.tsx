85787f67d55e748eca348213b0ab97a5714f6738
Pires <pires@email.com>
2026-03-02 18:33:43 -0300

    alteração telefone

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1428,7 +1428,7 @@ export default function ExpoTech2026() {
                         <svg className="w-5 h-5 text-[#00f3ff]" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                           <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M3 5a2 2 0 012-2h3.28a1 1 0 01.948.684l1.498 4.493a1 1 0 01-.502 1.21l-2.257 1.13a11.042 11.042 0 005.516 5.516l1.13-2.257a1 1 0 011.21-.502l4.493 1.498a1 1 0 01.684.949V19a2 2 0 01-2 2h-1C9.716 21 3 14.284 3 6V5z" />
                         </svg>
-                        <a href="https://wa.me/5511949703282?text=Olá!%20Gostaria%20de%20saber%20mais%20sobre%20o%20ExpoTech%202026!" target="_blank" rel="noopener noreferrer" className="text-[#00f3ff] hover:text-[#00d4e8] font-semibold">(11) 99470-3282</a>
+                        <a href="https://wa.me/5511949703282?text=Olá!%20Gostaria%20de%20saber%20mais%20sobre%20o%20ExpoTech%202026!" target="_blank" rel="noopener noreferrer" className="text-[#00f3ff] hover:text-[#00d4e8] font-semibold">(11) 9470-3282</a>
                       </div>
                     </div>
                   </div>
